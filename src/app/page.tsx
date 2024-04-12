d5a51025af6706e82c8cf20f6d5aded9f9eaad49
JeffreyJoel <jowoloko@gmail.com>
2024-04-12 04:53:49 +0100

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import { HomeAcurastSection } from '@/components/landing/home-page-acurast'
 import { HomeMidSection } from '@/components/landing/home-page-middle-section'
 import { HomeTopSection } from '@/components/landing/home-page-top-section'
 import { NavBar } from '@/components/shared/nav-bar'
+import { SparklesCore } from '@/components/ui/sparkles'
 // import { Partners } from './components/partners'
 
 export default function HomePage() {
@@ -23,14 +24,19 @@ export default function HomePage() {
   return (
     <>
       <NavBar />
-      <div className="blob  relative flex grow flex-col items-center justify-center py-10">
+      <div className="blob  relative flex grow flex-col items-center justify-center ">
         {/* Title */}
-        {/* <HomePageTitle /> */}
-        <HomeTopSection />
-
-        {/* <Partners /> */}
-
-        <HomeMidSection />
+        <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
 
         <HomeAcurastSection />
       </div>
