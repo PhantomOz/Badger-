8789032b28161dd2ad897d31f0c4f5d7b531c243
PhantomOz <faniogor@gmail.com>
2024-07-20 11:16:37 +0100

    This commit enables badger to work on vercel

--- src/contract_deployer/contracts/Lock.sol
+++ src/contract_deployer/contracts/Lock.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+contract GettingCode is ERC20 {
+    constructor() ERC20("Getting Code", "GCT") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
 }
